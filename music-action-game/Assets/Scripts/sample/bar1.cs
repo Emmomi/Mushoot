6e42915dfb3e79297dc79e1434788d2dec66b810
Emmomi <kgrowth12@gmail.com>
2021-07-13 14:13:10 +0900

    add all file

--- music-action-game/Assets/Scripts/sample/bar1.cs
+++ music-action-game/Assets/Scripts/sample/bar1.cs
@@ -8,7 +8,7 @@ public class bar1 : MonoBehaviour
     public GameObject mine;
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -16,29 +16,27 @@ public class bar1 : MonoBehaviour
     {
         transform.Translate(new Vector3(-0.1f, 0, 0));
         Vector3 pos = transform.position;
-        if (pos.x > -1 && pos.x < 1)
-        {
-            if (Input.GetKey(KeyCode.G))
-            {
+        if (pos.x>-1&&pos.x < 1) {
+            if (Input.GetKey(KeyCode.F)) {
                 Destroy(mine);
-                if (pos.x < 1 && pos.x > 0.7f || pos.x < -0.7f && pos.x > -1)
+                if (pos.x < 1 && pos.x > 0.7f|| pos.x < -0.7f && pos.x > -1)
                 {
                     sample_game.b++;
                     sample_game.c = 0;
                 }
-                else if (pos.x <= 0.7f && pos.x > 0.4f || pos.x < -0.4f && pos.x >= -0.7f)
+                else if (pos.x <= 0.7f && pos.x > 0.4f|| pos.x <-0.4f  && pos.x >= -0.7f)
                 {
                     sample_game.g++;
                     sample_game.c++;
                 }
-                else
-                {
+                else {
                     sample_game.p++;
                     sample_game.c++;
                 }
             }
         }
-        else if (pos.x < -2) {
+        else if (pos.x < -2)
+        {
             Destroy(mine);
             sample_game.b++;
             sample_game.c = 0;
